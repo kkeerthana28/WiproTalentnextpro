1da22a1eaf55ae71cf26cf424b079eaf2253250a
kkeerthana28 <89631321+kkeerthana28@users.noreply.github.com>
2023-02-06 23:19:29 +0530

    Create program8.java

---
+++ Day01/program8.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class loop5 {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+for(int i=100;i>=0;i--) {
+System.out.println(i);
+i=i-2;
+}
+}
+
+}
